7adeaa9f1c0fa03d2dc26bdf497967110c3c7f75
Mikel Garcia <mikel_6@hotmail.com>
2018-06-06 10:55:58 +0200

    Refactor constants in Title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,42 +1,22 @@
 import React from 'react';
 
 import { T } from '../../constants';
+import { TitleLineCurve } from './constants';
 import { pathFromBezierCurve } from '../../utils/formulas';
 import styles from './styles';
 
 const { TITLE } = T.COMPONENTS;
-const { textStyle } = styles;
+const { textStyle } = styles;
 
-const Title = () => {
-  const TitleLineCurve = {
-    initialAxis: {
-      x: -550,
-      y: -800,
-    },
-    initialControlPoint: {
-      x: 200,
-      y: -200,
-    },
-    endingControlPoint: {
-      x: 800,
-      y: -200,
-    },
-    endingAxis: {
-      x: 1150,
-      y: 0,
-    },
-  };
-
-  return (
-    <g filter="url(#shadow)">
-      <defs>
-        <path id="TitlePath" d={pathFromBezierCurve(TitleLineCurve)} />
-      </defs>
-      <text {...textStyle}>
-        <textPath xlinkHref="#TitlePath">{TITLE}</textPath>
-      </text>
-    </g>
-  );
-};
+const Title = () => (
+  <g filter="url(#shadow)">
+    <defs>
+      <path id="TitlePath" d={pathFromBezierCurve(TitleLineCurve)} />
+    </defs>
+    <text {...textStyle}>
+      <textPath xlinkHref="#TitlePath">{TITLE}</textPath>
+    </text>
+  </g>
+);
 
 export default Title;
---
+++ src/components/Title/constants.js
@@ -0,0 +1,18 @@
+export const TitleLineCurve = {
+  initialAxis: {
+    x: -550,
+    y: -800,
+  },
+  initialControlPoint: {
+    x: 200,
+    y: -200,
+  },
+  endingControlPoint: {
+    x: 800,
+    y: -200,
+  },
+  endingAxis: {
+    x: 1150,
+    y: 0,
+  },
+};
