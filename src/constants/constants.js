0a1377571c71fa4f33abd9fa816bb580471aa757
Mikel Garcia <mikel_6@hotmail.com>
2018-06-07 10:25:54 +0200

    Add utils/constants in constants/constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,11 @@
 export default {
+  CREATE_INTERVAL: 1000,
+  FLYING_OBJECTS_STARTER_POSITIONS: [-300, -150, 150, 300],
+  FLYING_OBJECTS_STARTER_Y_AXIS: -1100,
+  GAME_HEIGHT: 1200,
+  GAME_WIDTH: 800,
+  MAX_FLYING_OBJECTS: 4,
+  SKY_AND_GROUND_WIDTH: 5000,
   URLS: {
     CURRICULUM:
       'https://drive.google.com/file/d/1EZMS3LCtfdbMJgY1-heWxaFWII4fLciE/view?usp=sharing',
