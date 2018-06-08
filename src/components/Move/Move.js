323654bea5a3a85b959a3bb83ebe40dfcc19ccd9
Mikel Garcia <mikel_6@hotmail.com>
2018-06-08 12:07:43 +0200

    Add new Move component

---
+++ src/components/Move/Move.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import { C } from '../../constants';
+
+const { GAME_HEIGHT } = C;
+
+const moveVertically = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(${GAME_HEIGHT}px);
+  }
+`;
+
+const Move = styled.g`
+  animation: ${moveVertically} 4s linear;
+`;
+
+export default Move;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
 export { default as CannonBall } from './CannonBall';
 export { default as CannonBase } from './CannonBase';
+export { default as Move } from './Move';
 export { default as Title } from './Title';
