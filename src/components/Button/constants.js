34dd14b1aa50c644ad9c2b8808a099e6970c4158
Mikel Garcia <mikel_6@hotmail.com>
2018-06-08 10:40:39 +0200

    Refactor Styles in Button component

--- src/components/Button/constants.js
+++ src/components/Button/constants.js
@@ -1,6 +1,8 @@
 import { C } from '../../constants';
+import styles from './styles';
 
 const { GAME_WIDTH } = C;
+const { primaryStyle, secondaryStyle, textStyle } = styles;
 
 export const buttonSecondaryProps = {
   x: GAME_WIDTH  / -2,
@@ -8,27 +10,16 @@ export const buttonSecondaryProps = {
   height: 120,
   rx: 10, // border radius
   ry: 10, // border radius
-  style: {
-    fill: 'transparent',
-    cursor: 'pointer',
-  },
+  style: secondaryStyle,
 };
 
 export const buttonPrimaryProps = {
   ...buttonSecondaryProps,
-  style: {
-    ...buttonSecondaryProps.style,
-    fill: '#da0d15',
-  },
+  style: primaryStyle,
 };
 
 export const textProps = {
   textAnchor: 'middle',
   x: 0,
-  style: {
-    fontFamily: '"Joti One", cursive',
-    fontSize: 60,
-    fill: '#e3e3e3',
-    cursor: 'pointer',
-  },
+  style: textStyle,
 };
---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+export default {
+  primaryStyle: {
+    fill: '#da0d15',
+    cursor: 'pointer',
+  },
+  secondaryStyle: {
+    fill: 'transparent',
+    cursor: 'pointer',
+  },
+  textStyle: {
+    fontFamily: '"Joti One", cursive',
+    fontSize: 60,
+    fill: '#e3e3e3',
+    cursor: 'pointer',
+  },
+};
