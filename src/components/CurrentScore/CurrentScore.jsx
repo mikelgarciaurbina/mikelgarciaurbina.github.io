558420680afdb0d3ac928f20dd3b41851e4c579d
Mikel Garcia <mikel_6@hotmail.com>
2018-06-09 09:23:15 +0200

    Refactor styles in CurrentScore

--- src/components/CurrentScore/CurrentScore.jsx
+++ src/components/CurrentScore/CurrentScore.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CurrentScore = ({ score }) => {
-  const scoreStyle = {
-    fontFamily: '"Joti One", cursive',
-    fontSize: 80,
-    fill: '#d6d33e',
-  };
+import styles from './styles';
 
-  return (
-    <g filter="url(#shadow)">
-      <text style={scoreStyle} x="300" y="80">
-        {score}
-      </text>
-    </g>
-  );
-};
+const { scoreStyle } = styles;
+
+const CurrentScore = ({ score }) => (
+  <g filter="url(#shadow)">
+    <text style={scoreStyle} x="300" y="80">
+      {score}
+    </text>
+  </g>
+);
 
 CurrentScore.propTypes = {
   score: PropTypes.number.isRequired,
---
+++ src/components/CurrentScore/styles.js
@@ -0,0 +1,7 @@
+export default {
+  scoreStyle: {
+    fontFamily: '"Joti One", cursive',
+    fontSize: 80,
+    fill: '#d6d33e',
+  },
+};
