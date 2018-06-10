c08483531d05e7a979caaa3e50b405fc0b548fbc
Mikel Garcia <mikel_6@hotmail.com>
2018-06-10 15:13:23 +0200

    Refactor styles in Ground

--- src/components/Ground/Ground.jsx
+++ src/components/Ground/Ground.jsx
@@ -1,38 +1,30 @@
 import React from 'react';
 
-import { C } from '../../constants';
+import { C } from '../../constants';
+import styles from './styles';
 
 const { SKY_AND_GROUND_WIDTH } = C;
+const { divisionStyle, groundStyle } = styles;
 
-const Ground = () => {
-  const groundStyle = {
-    fill: '#59a941',
-  };
-  const division = {
-    stroke: '#458232',
-    strokeWidth: '3px',
-  };
-
-  return (
-    <g id="ground">
-      <rect
-        id="ground-2"
-        data-name="ground"
-        style={groundStyle}
-        x={SKY_AND_GROUND_WIDTH / -2}
-        y={0}
-        width={SKY_AND_GROUND_WIDTH}
-        height={100}
-      />
-      <line
-        x1={SKY_AND_GROUND_WIDTH / -2}
-        y1={0}
-        x2={SKY_AND_GROUND_WIDTH / 2}
-        y2={0}
-        style={division}
-      />
-    </g>
-  );
-};
+const Ground = () => (
+  <g id="ground">
+    <rect
+      id="ground-2"
+      data-name="ground"
+      style={groundStyle}
+      x={SKY_AND_GROUND_WIDTH / -2}
+      y={0}
+      width={SKY_AND_GROUND_WIDTH}
+      height={100}
+    />
+    <line
+      x1={SKY_AND_GROUND_WIDTH / -2}
+      y1={0}
+      x2={SKY_AND_GROUND_WIDTH / 2}
+      y2={0}
+      style={divisionStyle}
+    />
+  </g>
+);
 
 export default Ground;
---
+++ src/components/Ground/styles.js
@@ -0,0 +1,9 @@
+export default {
+  divisionStyle: {
+    stroke: '#458232',
+    strokeWidth: '3px',
+  },
+  groundStyle: {
+    fill: '#59a941',
+  },
+};
