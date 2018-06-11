516e6e9437b1b3f0077db85c1648405c999555fd
Mikel Garcia <mikel_6@hotmail.com>
2018-06-11 09:14:07 +0200

    Refactor styles in Sky

--- src/components/Sky/Sky.jsx
+++ src/components/Sky/Sky.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 
 import { C } from '../../constants';
+import styles from './styles';
 
 const { GAME_HEIGHT, SKY_AND_GROUND_WIDTH } = C;
+const { skyStyle } = styles;
 
-const Sky = () => {
-  const skyStyle = {
-    fill: '#30abef',
-  };
-
-  return (
-    <rect
-      style={skyStyle}
-      x={SKY_AND_GROUND_WIDTH / -2}
-      y={100 - GAME_HEIGHT}
-      width={SKY_AND_GROUND_WIDTH}
-      height={GAME_HEIGHT}
-    />
-  );
-};
+const Sky = () => (
+  <rect
+    style={skyStyle}
+    x={SKY_AND_GROUND_WIDTH / -2}
+    y={100 - GAME_HEIGHT}
+    width={SKY_AND_GROUND_WIDTH}
+    height={GAME_HEIGHT}
+  />
+);
 
 export default Sky;
---
+++ src/components/Sky/styles.js
@@ -0,0 +1,5 @@
+export default {
+  skyStyle: {
+    fill: '#30abef',
+  },
+};
